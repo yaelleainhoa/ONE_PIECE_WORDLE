173253aae9a92e94fc8a279e1ce47a887f151a1b
Yaelle Saralegui Tran <>
2024-03-16 14:55:48 +0100

    Policy.vue

---
+++ src/views/Policy.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+
+import Header from '@/components/Header.vue';
+
+</script>
+
+<template>
+    <div>
+      <Header></Header>
+      <div class="policy">
+        <p>
+            Legal Disclaimer
+
+The website OPDle utilizes images and information from the One Piece universe, a manga created by Eiichiro Oda. All manga and anime images are the property of their respective owners, including the manga published by Shueisha and the animation produced by TOEI Animation. No ownership claim is made on these elements.
+
+This site is created for non-profit purposes and is not intended to generate revenue. It aims to provide information and entertainment to One Piece fans. No financial gain is derived from its operation.
+
+If you are the copyright holder of any of the works used on this site and believe that their use constitutes a violation of your rights, please contact us at opdle@gmail.com so that we can resolve the issue promptly.
+
+Thank you for your understanding.
+        </p>
+      </div>
+    </div>
+  </template>
